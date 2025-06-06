eb71c4d27366e33c94a2c295255e76a488d1a084
Aashish518 <jadavaashish9@gmail.com.com>
2025-06-06 22:34:20 +0530

    nnn

--- backend/index.js
+++ backend/index.js
@@ -19,8 +19,8 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieparser());
 
 
