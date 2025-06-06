cd2db2d28e8d98f4298509cb1d38ea031f0a0c44
Aashish518 <jadavaashish9@gmail.com.com>
2025-06-06 21:53:11 +0530

    aaa

--- frontend/src/pages/BooksPage.jsx
+++ frontend/src/pages/BooksPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "../components-css/BookCard.css";
+import "../components-css/Bookard.css";
 import Load from "../components/Load";
 import { Bookcard } from "../components/Bookcard";
 import { FilterComponent } from "../components/Filter";
